9de329cf96bb66d0e438c6c9e6a39cc748ae9df7
Egor Bakhorikov <egorbakhorikov.work@gmail.com>
2023-02-17 20:31:46 +0300

    Done first task. (sort)

---
+++ Homework1/InsertionSort/InsertionSort/InsertionSort.cs
@@ -0,0 +1,66 @@
+﻿using static System.Console;
+
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            Write("Enter the size of array (positive number): ");
+            int size = -1;
+            while (!int.TryParse(ReadLine(), out size) || size <= 0)
+            {
+                WriteLine("Wrong size. Must be a positive number.");
+                Write("Enter the size of array (positive number): ");
+            }
+
+            WriteLine("Enter array elements one by one (integer numbers). ");
+
+            int[] array = new int[size];
+            for (int i = 0; i < size; ++i)
+            {
+                int value;
+                while (!int.TryParse(ReadLine(), out value))
+                {
+                    WriteLine("Wrong value");
+                }
+
+                array[i] = value;
+            }
+
+            SortArrayByInsertionSort(array);
+
+            Write("Sorted array: ");
+
+            for (int i = 0; i < size; ++i)
+            {
+                Write($"{array[i]} ");
+            }
+        }
+
+
+        static int[] SortArrayByInsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j - 1] > array[j])
+                {
+                    Swap(array, j - 1, j);
+                    j--;
+                }
+            }
+
+            return array;
+        }
+
+
+        static void Swap(int[] array, int firstIndex, int secondIndex)
+        {
+            int tempValue = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = tempValue;
+        }
+    }
+}
