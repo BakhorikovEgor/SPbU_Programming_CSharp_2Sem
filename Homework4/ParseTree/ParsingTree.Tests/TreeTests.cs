2c34ae636c2c74c3dcf00af9aca69feda59414db
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-03-29 19:57:06 +0300

    All tests done.

--- Homework4/ParseTree/ParsingTree.Tests/TreeTests.cs
+++ Homework4/ParseTree/ParsingTree.Tests/TreeTests.cs
@@ -2,15 +2,16 @@ namespace ParsingTreeTests;
 
 public class TreeTests
 {
-
     private bool DoubleEquality(double x, double y)
         => Math.Abs(x - y) < 0.0001;
 
-    [Test]
-    public void CorrectExpressionTest_ShouldReturnTrueValue()
+    [TestCase("(/ (* (- 2 5) (/ (+ 10 5) (* 5 1))) 3)", -3.0)]
+    [TestCase("(/ (* 10 2) (+ 3 4))", 2.8571)]
+    [TestCase("(* 0 0)", 0.0)]
+    [TestCase("(- 123 34", 89.0)]
+    [TestCase("(+ 1 1)", 2.0)]
+    public void CorrectExpressionTest_ShouldReturnExpectedValue(string expression, double expected)
     {
-        var expected = -3;
-        var expression = "(/ (* (- 2 5) (/ (+ 10 5) (* 5 1))) 3)";
         var tree = new Tree(expression);
 
         var actual = tree.Calculate();
@@ -19,7 +20,7 @@ public class TreeTests
     }
 
     [Test]
-    public void CorrectInputWithoutBrackets_ShouldReturnTrueValue()
+    public void CorrectExpressionWithoutBrackets_ShouldReturnExpectedValue()
     {
         var expected = 1;
         var expression = "* / 2 4 / 4 2";
@@ -39,16 +40,17 @@ public class TreeTests
         Assert.Throws<DivideByZeroException>(() => tree.Calculate());
     }
 
-    [Test]
-    public void EmptyExpression_ShouldThrowException()
-        => Assert.Throws<WrongExpressionException>(() => new Tree(string.Empty));
+    [TestCase("(+ 1 )")]
+    [TestCase("( - 1 1)")]
+    [TestCase("(1 1 +)")]
+    public void InvalidInputFormat_ShouldThrowException(string expression)
+        => Assert.Throws<WrongExpressionException>(() => new Tree(expression));
 
     [Test]
-    public void InvalidInputFormat_ShouldThrowException()
-        => Assert.Throws<WrongExpressionException>(() => new Tree("( + 1 )"));
+    public void EmptyExpression_ShouldThrowException()
+     => Assert.Throws<WrongExpressionException>(() => new Tree(string.Empty));
 
     [Test]
     public void InvalidOperationInExpression_ShouldThrowException()
         => Assert.Throws<WrongExpressionException>(() => new Tree("(+ (- 1 1) (^ 2 2))"));
-
 }
