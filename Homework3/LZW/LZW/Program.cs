a51c67041e0dfbfba2c5c8f2f5a1ba6dce2e176e
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-03-29 22:53:05 +0300

    Added the ability to get help about the program using - help.

--- Homework3/LZW/LZW/Program.cs
+++ Homework3/LZW/LZW/Program.cs
@@ -1,19 +1,66 @@
-﻿using LZW;
+﻿using static System.Console;
+using LZW;
 
+
+if (args[0] == "-help")
+{
+    WriteLine("""
+        This program is for compressing binary files using the Lempel-Ziv-Welch and Burrows-Wheeler algorithms.
+        How to use:
+
+        To compress:
+        - - - - - - - - - - - - - - - - - - - - - - - - 
+        dotnet run [file path] --c
+        (If the path contains spaces -> enclose it in quotes)
+        - - - - - - - - - - - - - - - - - - - - - - - - 
+        
+        To decompress:
+        - - - - - - - - - - - - - - - - - - - - - - - - 
+        dotnet run [file(.zipped) path] --u
+        (If the path contains spaces -> enclose it in quotes)
+        - - - - - - - - - - - - - - - - - - - - - - - - 
+        """);
+    return 0;
+}
 
 if (args.Length < 2)
 {
-    Console.WriteLine("Less than 2 arguments");
+    WriteLine("Two arguments are needed");
+    WriteLine("For help, use the command: dotnet run -help");
 }
-else if (args[1] == "-c")
+
+else if (args[1] == "--c")
 {
-    CompressHandler.CompressWithAndWithoutBWT(args[0]);
+    WriteLine("Handling...");
+    try
+    {
+        CompressHandler.CompressWithAndWithoutBWT(args[0]);
+        WriteLine("\nDone");
+    }
+    catch (FileNotFoundException)
+    {
+        WriteLine("No such file");
+        WriteLine("For help, use the command: dotnet run -help");
+    }
 }
-else if (args[1] == "-u")
+else if (args[1] == "--u")
 {
-    CompressHandler.Decompress(args[0]);
+    WriteLine("Handling...");
+    try
+    {
+        CompressHandler.Decompress(args[0]);
+        WriteLine("\nDone");
+    }
+    catch (FileNotFoundException)
+    {
+        WriteLine("No such file");
+        WriteLine("For help, use the command: dotnet run -help");
+    }
 }
 else
 {
-    Console.WriteLine("Second argument isn`t correct !");
+    WriteLine("Second argument isn`t correct !");
+    WriteLine("For help, use the command: dotnet run -help");
 }
+
+return 0;
