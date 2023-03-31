70f908a25fa8b743f424121944d185822476eba6
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-03-31 20:36:36 +0300

    add documentation

--- Homework6/Functional Methods/FunctionalMethods.cs
+++ Homework6/Functional Methods/FunctionalMethods.cs
@@ -2,8 +2,15 @@
 
 public static class FunctionalMethods
 {
-
-    public static List<R> Map<T, R> (List<T> list, Func<T,R> function)
+    /// <summary>
+    /// Applies the specified mapping to each element of the list.
+    /// </summary>
+    /// <typeparam name="T"> Type of elements in list. </typeparam>
+    /// <typeparam name="R"> The type of the result after applying the mapping. </typeparam>
+    /// <param name="list"> List of elements to transform. </param>
+    /// <param name="function"> Element transform function. </param>
+    /// <returns> List of converted elements of type R. </returns>
+    public static List<R> Map<T, R> (List<T> list, Func<T, R> function)
     {
         var result = new List<R>();
         foreach (var element in list)
@@ -14,6 +21,13 @@ public static class FunctionalMethods
         return result;
     }
 
+    /// <summary>
+    /// Filters the elements of the list by the given predicate.
+    /// </summary>
+    /// <typeparam name="T"> Type of elements in list. </typeparam>
+    /// <param name="list"> List of elements to filter. </param>
+    /// <param name="predicate"> Function that takes a value and returns a bool.</param>
+    /// <returns> List of elements whose predicate evaluates to true. </returns> 
     public static List<T> Filter<T> (List<T> list, Func<T, bool> predicate)
     {
         var result = new List<T>();
@@ -27,7 +41,16 @@ public static class FunctionalMethods
         return result;
     }
 
-    public static V Fold<T,V> (List<T> list,V accumulatedElement, Func<V,T,V> binaryFunction)
+    /// <summary>
+    /// Accumulate the original value by the given function, passing through the list.
+    /// </summary>
+    /// <typeparam name="T"> Type of elements in list. </typeparam>
+    /// <typeparam name="V"> Type of accumulating element. </typeparam>
+    /// <param name="list"> List of elements for accumulation. </param>
+    /// <param name="accumulatedElement"> Element we want to accumulate passing through the list. </param>
+    /// <param name="binaryFunction"> Function of two arguments, with a non void return value</param>
+    /// <returns></returns>
+    public static V Fold<T, V> (List<T> list,V accumulatedElement, Func<V, T, V> binaryFunction)
     {
         foreach (var element in list)
         {
