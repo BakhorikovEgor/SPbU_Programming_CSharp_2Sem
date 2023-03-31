35dac5d458b4f0c72eb7bbc2afd1eaa7e1e7c452
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-03-31 20:21:01 +0300

    Created main functional for task 1

---
+++ Homework6/Functional Methods/FunctionalMethods.cs
@@ -0,0 +1,39 @@
+﻿namespace Func;
+
+public static class FunctionalMethods
+{
+
+    public static List<R> Map<T, R> (List<T> list, Func<T,R> function)
+    {
+        var result = new List<R>();
+        foreach (var element in list)
+        {
+            result.Add(function(element));
+        }
+   
+        return result;
+    }
+
+    public static List<T> Filter<T> (List<T> list, Func<T, bool> predicate)
+    {
+        var result = new List<T>();
+        foreach (var element in list)
+        {
+            if (predicate(element))
+            {
+                result.Add(element);
+            }
+        }
+        return result;
+    }
+
+    public static V Fold<T,V> (List<T> list,V accumulatedElement, Func<V,T,V> binaryFunction)
+    {
+        foreach (var element in list)
+        {
+            accumulatedElement = binaryFunction(accumulatedElement, element);
+        }
+
+        return accumulatedElement;
+    }
+}
