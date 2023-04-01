a35286535184978ef5a4608b312b9ea746c2e634
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-04-01 20:56:18 +0300

    Started Block class doing

--- Homework6/Tetris/Tetris/TetrisRealization/Block.cs
+++ Homework6/Tetris/Tetris/TetrisRealization/Block.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris.TetrisRealization
+﻿namespace TetrisRealization;
+internal static class BlockCreator
 {
-    internal class Block
+    private static readonly (int, int)[][] BLOCKS = new (int, int)[][]
     {
+        new(int, int)[] { (0, 0), (0,1), (0, 2), (-1 ,2) },
+        new(int,int) [] { (0, 0), (0,1), (0,2) , (0,3) },
+        new(int, int)[] { (0, 0), (0,1), (1, 0), (1 ,1) },
+        new(int, int)[] { (0, 0), (0, 1), (0, 2), (1 ,2) },
+        new(int, int)[] { (0, 0), (0, 1), (1, 1), (2 ,1) },
+        new(int, int)[] { (0, 0), (1, 0), (1, -1), (2 ,-1) },
+        new(int, int)[] { (0, 0), (1, 0), (1, -1), (2, 0)}
+    };
+
+    private static readonly Random random = new Random();
+
+    public static (int, int)[] CreateBlock(int x, int y)
+    {
+        var block = new List<(int, int)> ();
+        foreach (var blockElement in BLOCKS[random.Next(0, BLOCKS.Length)]) 
+        {
+            block.Add((blockElement.Item1 + x, blockElement.Item2 + y));
+        }
+
+        return block.ToArray();
     }
 }
+
