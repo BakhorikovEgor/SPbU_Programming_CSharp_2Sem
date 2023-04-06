e35702f64236fdb86c5e621c609e0b452f1d1f55
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-04-06 17:33:30 +0300

    Creating game field class

--- Homework6/Tetris/Tetris/TetrisRealization/Block.cs
+++ Homework6/Tetris/Tetris/TetrisRealization/Block.cs
@@ -1,28 +1,20 @@
-﻿namespace TetrisRealization;
+﻿namespace Tetris.Realization;
 internal static class BlockCreator
 {
     private static readonly (int, int)[][] BLOCKS = new (int, int)[][]
     {
-        new(int, int)[] { (0, 0), (0,1), (0, 2), (-1 ,2) },
-        new(int,int) [] { (0, 0), (0,1), (0,2) , (0,3) },
-        new(int, int)[] { (0, 0), (0,1), (1, 0), (1 ,1) },
+        new(int, int)[] { (0, 0), (0, 1), (0, 2), (-1 ,2) },
+        new(int, int)[] { (0, 0), (0, 1), (0, 2) , (0, 3) },
+        new(int, int)[] { (0, 0), (0, 1), (1, 0), (1 ,1) },
         new(int, int)[] { (0, 0), (0, 1), (0, 2), (1 ,2) },
         new(int, int)[] { (0, 0), (0, 1), (1, 1), (2 ,1) },
         new(int, int)[] { (0, 0), (1, 0), (1, -1), (2 ,-1) },
         new(int, int)[] { (0, 0), (1, 0), (1, -1), (2, 0)}
     };
 
-    private static readonly Random random = new Random();
+    private static readonly Random random = new();
 
     public static (int, int)[] CreateBlock(int x, int y)
-    {
-        var block = new List<(int, int)> ();
-        foreach (var blockElement in BLOCKS[random.Next(0, BLOCKS.Length)]) 
-        {
-            block.Add((blockElement.Item1 + x, blockElement.Item2 + y));
-        }
-
-        return block.ToArray();
-    }
+        => BLOCKS[random.Next(0, BLOCKS.Length)].Select(point => (point.Item1 + x, point.Item2 + y)).ToArray();
 }
 
