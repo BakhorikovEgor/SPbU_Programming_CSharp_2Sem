1828570ceb9520e6d51044cc88536995f9978619
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-04-09 12:37:13 +0300

    eventloop fixes

--- Homework6/Tetris/Tetris/Execution/EventLoop.cs
+++ Homework6/Tetris/Tetris/Execution/EventLoop.cs
@@ -1,18 +1,53 @@
 ﻿using static System.Console;
 
-namespace Tetris.Realization;
 
-public class Executor
+namespace Tetris.Execution;
+
+public class EventLoop
 {
 
-    public void Run(int length, int width)
+    public event EventHandler<EventArgs> GameEndingHandler = (sender, eventArgs) => { };
+
+    public event EventHandler<EventArgs> LandingHandler = (sender, eventArgs) => { };
+
+    public event EventHandler<EventArgs> RotateHandler = (sender, eventArgs) => { };
+
+    public event EventHandler<EventArgs> RightShiftHandler = (sender, eventArgs) => { };
+
+    public event EventHandler<EventArgs> LeftShiftHandler = (sender, eventArgs) => { };
+
+    public event EventHandler<EventArgs> DownShiftHandler = (sender, eventArgs) => { };
+
+    public void Run()
     {
-        Field field = new Field(length, width);
-        var processing = true;
-        
-        field.AddCurrentBlock();
-        field.MoveCurrentBlock((0, 1));
-        field.RotateCurrentBlock();
-        field.MoveCurrentBlock((0, 1));
+        while(true) 
+        {
+            if (KeyAvailable)
+            {
+                var key = ReadKey().Key;         
+                switch (key)
+                {
+                    case ConsoleKey.Escape:
+                        GameEndingHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.W:
+                        RotateHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.S:
+                        DownShiftHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.D:
+                        RightShiftHandler(this, EventArgs.Empty);
+                        break;
+
+                    case ConsoleKey.A:
+                        LeftShiftHandler(this, EventArgs.Empty);
+                        break;
+                }
+            }
+        }
     }
 }
--- Homework6/Tetris/Tetris/Execution/Executor.cs
+++ Homework6/Tetris/Tetris/Execution/Executor.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Tetris.Realization;
 
-namespace Tetris.Execution
+namespace Tetris.Execution;
+
+public class Executor
 {
-    internal class Executor
-    {
-    }
+    public Executor();
 }
