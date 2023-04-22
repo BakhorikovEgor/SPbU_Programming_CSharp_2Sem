54a998dfe9913c2fad2b0c6169fe837abcb6a279
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-04-22 14:36:49 +0300

    add contains method

--- Homework8/SkipList/SkipList/SkipList.cs
+++ Homework8/SkipList/SkipList/SkipList.cs
@@ -2,29 +2,46 @@
 
 namespace SkipListRealization;
 
-internal class SkipList<T> where T : IComparable<T>
+internal class SkipList<T> : IList<T>  where T : IComparable<T>
 { 
     class SkipListNode
     {
         public SkipListNode[] Next { get; set; }
 
-        Random _rand = new Random();
-        public T Key { get; }
+        public T? Key { get; }
 
-        public SkipListNode(T key, int level)
+        public SkipListNode(T? key, int level)
         {
             Key = key;     
             Next = new SkipListNode[level];
         }
+    }
 
-        public int CompareTo(SkipListNode? other)
+    static readonly int _maxLevel = 33;
+    static readonly Random random = new();
+
+    int _currentMaxLevel = 1;
+    SkipListNode _head = new(default, _maxLevel);
+
+
+    public bool Contains(T value)
+    {
+        SkipListNode currentNode = _head;
+        for (var level = _maxLevel - 1; level >= 0; level--)
         {
-            if (other == null)
+            while (currentNode.Next[level] != null)
             {
-                throw new ArgumentException();
+                if (value.CompareTo(currentNode.Next[level].Key) < 0)
+                {
+                    break;
+                }
+                if (value.CompareTo(currentNode.Next[level].Key) == 0)
+                {
+                    return true;
+                }
             }
-            return Key.CompareTo(other.Key);
         }
+        return false;
     }
 
 }
