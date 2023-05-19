f4f23a24a830c16bde1a57492d6edeef3b83951a
BakhorikovEgor <egorbakhorikov.work@gmail.com>
2023-05-19 14:05:48 +0300

    add compressor

---
+++ FinalTasks/Task1/Task1/Task1/Compressor.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Security.Cryptography;
+
+namespace Task1;
+
+internal static class Compressor
+{ 
+    private static readonly byte _maxByte = 255;
+    public static (byte[],double) Compress(byte[] data)
+    {
+
+        var result = new List<byte>();
+
+        byte previousByte = 0;
+        byte counter = 0;
+        foreach (byte b in data)
+        {
+            if (b == previousByte)
+            {
+                if (counter == _maxByte)
+                {
+                    result.Add(_maxByte);
+                    result.Add(b);
+                    counter = 0;
+                }
+                counter++;
+            }
+            else
+            {
+                if (counter > 1)
+                {
+                    result.Add(counter);
+                }
+                result.Add(previousByte);
+
+                counter = 1;
+                previousByte = b;
+            }
+        }
+
+        return (result.ToArray(), (double)data.Length / result.Count);
+    }
+}
---
+++ FinalTasks/Task1/Task1/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task1;
+
+
+byte[] bytes = { 0,0, 1,1, 2,2, 3,3, 4,4, 5,5 };
+
+var b = Compressor.Compress(bytes);
+Console.WriteLine(b.Item2);
