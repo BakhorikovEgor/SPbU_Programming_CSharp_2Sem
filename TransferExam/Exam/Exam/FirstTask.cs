9414d66ffb2b324f0bb8229ea4f211909d6d39da
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-08-14 13:02:49 +0300

    made realization for int array

---
+++ TransferExam/Exam/Exam/FirstTask.cs
@@ -0,0 +1,28 @@
+﻿namespace Exam;
+
+public static class FirstTask
+{
+    public static int FindMostFrequentElement(int[] array)
+    {
+        var dict = new Dictionary<int, int>();
+
+        int mostFrequentItem = -1;
+        int maxFrequency = -1;
+
+        foreach (int item in array)
+        {
+            dict[item] = dict.ContainsKey(item) 
+                ? dict[item]++ 
+                : 1;
+
+            if (dict[item] > maxFrequency)
+            {
+                maxFrequency = dict[item];
+                mostFrequentItem = item;
+            }
+
+        }
+
+        return mostFrequentItem;
+    }
+}
