0cabe2eb0b7832ef51e47ca09d3cdce5f142cdf3
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-08-14 13:33:27 +0300

    previous commit fix

--- TransferExam/Exam/Exam/LinkedListStack.cs
+++ TransferExam/Exam/Exam/LinkedListStack.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Exam;
 
-namespace Exam
+/// <summary>
+/// Stack realization by using LinkedList structure.
+/// </summary>
+public class LinkedListStack 
 {
-    internal class LinkedListStack
+    private class Node
     {
+        public char Value { get; }
+        public Node? Next { get; }
+
+        public Node(char value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public Node(char value, Node? next)
+        {
+            Value = value;
+            Next = next;
+        }
     }
+
+    private Node? head;
+
+    public void Push(char value)
+        => head = head == null
+                ? new Node(value)
+                : new Node(value, head);
+
+    public char Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Can`t pop from empty stack");
+        }
+
+        char returnValue = head.Value;
+        head = head.Next == head 
+            ? null 
+            : head.Next;
+
+        return returnValue;
+    }
+
+    public bool IsEmpty()
+        => head == null;
 }
--- TransferExam/Exam/Exam/SecondTask.cs
+++ TransferExam/Exam/Exam/SecondTask.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Exam;
 
-namespace Exam
+public static class SecondTask
 {
-    internal class SecondTask
+
+    public static bool IsBalanced(string input)
     {
+        var stack = new LinkedListStack();
+        foreach (var item in input)
+        {
+            if (IsOpeningBracket(item))
+            {
+                stack.Push(item);
+            }
+            else if (IsClosingBracket(item))
+            {
+                if (stack.IsEmpty() || !CheckMatching(stack.Pop(), item))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.IsEmpty();
     }
+
+    static bool IsOpeningBracket(char c)
+        => c == '(' || c == '[' || c == '{';
+
+    static bool IsClosingBracket(char c)
+        => c == ')' || c == ']' || c == '}';
+
+    static bool CheckMatching(char opening, char closing)
+        => (opening == '(' && closing == ')') ||
+           (opening == '[' && closing == ']') ||
+           (opening == '{' && closing == '}');
 }
