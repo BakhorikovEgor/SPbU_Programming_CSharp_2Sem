fc2b0fcf35bf16badd9c53b46233bbd176b845ab
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-08-14 13:10:13 +0300

    simple test for first task

---
+++ TransferExam/Exam/TransferExamTests/FirstTasksTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace TransferExamTests;
+
+public class FirstTasksTests
+{
+    [TestCase(new int[] {1, 1, 1, 2, 3, 4}, 1)]
+    public void FindMostFrequentElementTest(int[] array, int result)
+        => Assert.That(FirstTask.FindMostFrequentElement(array).Equals(result), Is.True);
+}
