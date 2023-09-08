a215b1ea3a94262e8d69b46a24b7c55e81a07795
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-09-08 23:12:48 +0300

    feat: create Matrix class with method for standart matrix multiplication

---
+++ TransferExam/MatrixMultiplication/MatrixMultiplication/Matrix.cs
@@ -0,0 +1,48 @@
+﻿namespace MatrixMultiplication;
+
+public class Matrix
+{
+    public int[,] MatrixData { get; private set; }
+    public int Rows { get; private set; }
+    public int Columns { get; private set; }
+
+    public Matrix(int[,] data)
+    {
+        MatrixData = data;
+        Rows = data.GetLength(0);
+        Columns = data.GetLength (1);
+    }
+
+    public Matrix Multiply(Matrix secondMatrix)
+    {
+        if (!MultiplicationMatching(secondMatrix))
+        {
+            throw new ArgumentException();
+        }
+
+        var newMatrixData = new int[Rows, secondMatrix.Columns];
+        for (var i = 0; i < Rows; ++i)
+        {
+            for (var j = 0; j < secondMatrix.Columns; ++j)
+            {
+                for (var k = 0; k < Columns; ++k)
+                {
+                    newMatrixData[i, j] += MatrixData[i, k] * secondMatrix.MatrixData[k, j];
+                }
+            }
+        }
+
+        var newMatrix = new Matrix(newMatrixData);
+        return newMatrix;
+    }
+
+    bool MultiplicationMatching (Matrix secondMatrix)
+    {
+        if (Columns != secondMatrix.Rows)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
